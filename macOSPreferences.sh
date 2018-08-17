41e46c17f425e76f66965c7df209256d3a8429ba
Connor Sanders <c.ellemsanders@gmail.com>
2018-08-17 16:01:35 +1000

    Inital update of macOS Preferences

--- macOSPreferences.sh
+++ macOSPreferences.sh
@@ -10,3 +10,30 @@ sudo -v
 
 # Keep Alive (Thanks mathiasbynes for this ;))
 while true; do sudo -n true; sleep 60; kill -o $$ || exit; done 2>/dev/null &
+
+
+# Set Computer name
+compName="Connor's Macbook Pro"
+sudo scutil --set ComputerName "$compName"
+sudo scutil --set HostName "$compName"
+sudo scutil --set LocalHostName "$compName"
+sudo defaults write /Library/Preferences/SystemConfiguration/com.apple.smb.server NetBIOSName -string "$compName"
+
+
+# Expand Save Panel by default
+defaults write NSGlobalDomain NSNavPanelExpandedStateForSaveMode -bool true
+defaults write NSGlobalDomain NSNavPanelExpandedStateForSaveMode2 -bool true
+
+# Save to HDD, not iCloud default
+defaults write NSGlobalDomain NSDocumentsSaveNewDocumentsToCloud -bool true
+
+# Trackpad: enable tap to click for this user and for the login screen
+defaults write com.apple.driver.AppleBluetoothMultitouch.trackpad Clicking -bool true
+defaults -currentHost write NSGlobalDomain com.apple.mouse.tapBehavior -int 1
+defaults write NSGlobalDomain com.apple.mouse.tapBehavior -int 1
+
+# Trackpad: Enable 3 Finger Drag
+defaults write com.apple.AppleMultitouchTrackpad.plist TrackpadThreeFingerDrag -bool YES
+
+# Set the timezone; see `sudo systemsetup -listtimezones` for other values
+sudo systemsetup -settimezone "Australia/Sydney" > /dev/null
