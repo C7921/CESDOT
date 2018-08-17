3dd94fd9d4bceb0af55a2c2882c78fa3d478c136
Connor Sanders <c.ellemsanders@gmail.com>
2018-08-17 14:05:30 +1000

    BrewFile & pref Start
    Added brew file. Began prefs with sudo keep alive

--- macOSPreferences.sh
+++ macOSPreferences.sh
@@ -1,3 +1,12 @@
 #!/usr/bin/env bash
 
-# Installs and updates macOS preferences
+#### Sets macOS Preferences
+
+# Quit System Preferences
+osascript -e 'tell application "System Prefernces" to quit'
+
+# Get Administrator rights now
+sudo -v
+
+# Keep Alive (Thanks mathiasbynes for this ;))
+while true; do sudo -n true; sleep 60; kill -o $$ || exit; done 2>/dev/null &
