f959c859bf9b00876f21dd41d03bab24267ed48a
C7921 <c.ellemsanders@gmail.com>
2018-08-20 10:39:40 +1000

    sourcing install files

--- install_main.sh
+++ install_main.sh
@@ -1,2 +1,10 @@
 #!/usr/bin/env bash
 # Kicks off the setup for CESDOT
+
+
+echo "Beginning Setup..."
+
+# Installs and run dotfile scripts
+source /Homebrew/homebrewInstall.sh
+source /Apps/appInstall.sh
+source /macOS/macOs.sh
