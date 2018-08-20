1298327956ec52acbc827efbe77e35ceb930485a
C7921 <c.ellemsanders@gmail.com>
2018-08-20 10:40:07 +1000

    Purpose comment

--- Homebrew/homebrewInstall.sh
+++ Homebrew/homebrewInstall.sh
@@ -1,5 +1,6 @@
 #!/usr/bin/env bash
 
+# Checks for Homebrew and install if not found.
 if test ! $(which brew); then
   echo "Installing Homebrew"
   ruby -e "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/master/install)"
